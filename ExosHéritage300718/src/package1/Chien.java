db8d0610a6e8548fe1055ca14ea0451b9394dc07
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-07-30 10:44:01 +0200

    first commit upload all files

---
+++ ExosHéritage300718/src/package1/Chien.java
@@ -0,0 +1,9 @@
+package package1;
+
+// Le mot clé extends sert de lien pour l'héritage
+// On met le nom de ma classe (Chien) + le mot clé (extends) + le nom de la classe parent (Animal)
+public class Chien extends Animal {
+	public String aboie() {
+		return "OUAF OUAF";
+	}
+}
---
+++ ExosHéritage300718/src/package1/CreateAnimal.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class CreateAnimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Chien = monChien = new Chien();
+		monChien.setNom ("Medor");
+		monChien.setCouleur("Noir");
+		System.out.println("Mon chien s'appelle " + monChien.getNom() +" et il fait "+monChien.aboie());
+	}
+
+}
